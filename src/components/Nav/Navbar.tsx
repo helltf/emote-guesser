45be1f1cffb13b9eec9a695fe358f20b9d7a6aab
helltf <dario.zachow5530@gmail.com>
2023-01-23 20:11:14 +0100

    chore: add links

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 import icon from "../../../public/emote_guesser_icon.gif";
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-gray-500 h-[80px] flex flex-row px-20 items-center">
+    <nav className="w-full bg-gray-500 h-[80px] flex flex-row px-20 items-center justify-between">
       <div className="flex flex-row h-[64px] align-middle gap-2">
         <Image
           className=""
@@ -12,7 +13,14 @@ export default function Navbar() {
           width="64"
           height="64"
         ></Image>
-        <p className="text-5xl text-center">Emote Guesser</p>
+        <p className="text-5xl text-center m-0 text-purple-700 italic">
+          Emote Guesser
+        </p>
+      </div>
+      <div>
+        <Link href="/play">Play</Link>
+        <Link href="/about">About</Link>
+        <Link href="/login">Login</Link>
       </div>
     </nav>
   );
