253af4fe5a940c7629f4e128e7f990f285272142
helltf <dario.zachow5530@gmail.com>
2023-01-25 18:44:05 +0100

    chore: formatting home component

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,9 @@
-import Hero from "./Hero";
+import Hero from './hero';
 
 export default function Home() {
-  return <div className="h-[calc(100vh-60px)] bg-neutral-800">
-    <Hero></Hero>
-  </div>;
+  return (
+    <div className="h-[calc(100vh-60px)] bg-neutral-800">
+      <Hero></Hero>
+    </div>
+  );
 }
