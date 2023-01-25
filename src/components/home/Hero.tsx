b14386dd4bce4313a82834f19babfda9337165da
helltf <dario.zachow5530@gmail.com>
2023-01-25 18:43:38 +0100

    feat: use hero button for links

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,23 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import typingGif from '../../../public/typing.gif';
+import HeroButton from './hero-button';
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className="flex flex-col items-center justify-center h-full gap-1">
       <Image alt="typing gif" width="384" height="128" src={typingGif}></Image>
       <div className="flex flex-col w-[30%]">
-        <h1 className="text-purple-600 text-8xl italic self-start">Emote</h1>
+        <h1 className="text-purple-600 text-8xl italic self-start font-serif">
+          Emote
+        </h1>
         <h1 className="text-purple-600 text-8xl italic self-end"> Guesser</h1>
       </div>
       <div className="flex gap-2">
-        <Link
-          className="px-8 p-3 text-white text-2xl border-2 border-white m-4 hover:bg-white hover:text-purple-600 transition duration-500"
-          href="/play"
-        >
-          Play a game
-        </Link>
-        <Link href="/stats">Review statistics</Link>
+        <HeroButton href="/play">Play a game</HeroButton>
+        <HeroButton href="/stats">Review statistics</HeroButton>
       </div>
     </div>
   );
