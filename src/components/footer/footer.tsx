531988941180da95c9d38f30a884c0a221ae7ec4
helltf <dario.zachow5530@gmail.com>
2023-01-26 18:34:58 +0100

    refator: move icons into seperate component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,33 +1,13 @@
 import NavIcons from '../nav/nav-icons';
+import FooterIcons from './footer-icons';
 
 export default function Footer() {
   return (
     <div className="px-8 flex justify-between items-center h-[60px] bg-neutral-800  ">
-      <p className="text-neutral-500">Created by @helltf</p>
+      <p className="text-gray-500">Created by @helltf</p>
       <div className="flex gap-2">
         <p className="text-gray-500">Built with </p>
-        <svg
-          className="fill-gray-500 transition hover:fill-white"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="24"
-          height="24"
-          viewBox="0 0 48 48"
-        >
-          <path d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"></path>
-        </svg>
-        <svg
-          className="hover:fill-[#00acc1] transition fill-gray-500"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="24"
-          height="24"
-          viewBox="0 0 48 48"
-        >
-          <path d="M24,9.604c-6.4,0-10.4,3.199-12,9.597c2.4-3.199,5.2-4.398,8.4-3.599 c1.826,0.456,3.131,1.781,4.576,3.247C27.328,21.236,30.051,24,36,24c6.4,0,10.4-3.199,12-9.598c-2.4,3.199-5.2,4.399-8.4,3.6 c-1.825-0.456-3.13-1.781-4.575-3.247C32.672,12.367,29.948,9.604,24,9.604L24,9.604z M12,24c-6.4,0-10.4,3.199-12,9.598 c2.4-3.199,5.2-4.399,8.4-3.599c1.825,0.457,3.13,1.781,4.575,3.246c2.353,2.388,5.077,5.152,11.025,5.152 c6.4,0,10.4-3.199,12-9.598c-2.4,3.199-5.2,4.399-8.4,3.599c-1.826-0.456-3.131-1.781-4.576-3.246C20.672,26.764,17.949,24,12,24 L12,24z"></path>
-        </svg>
+        <FooterIcons />
       </div>
     </div>
   );
