90b7522053a93a3fca8592c80467609224c13ce3
helltf <dario.zachow5530@gmail.com>
2023-01-26 18:12:45 +0100

    chore: adjust document to fit footer

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,7 +2,7 @@ import Hero from './hero';
 
 export default function Home() {
   return (
-    <div className="h-[calc(100vh-60px)] bg-neutral-800">
+    <div className="h-[calc(100vh-120px)] bg-neutral-800">
       <Hero></Hero>
     </div>
   );
