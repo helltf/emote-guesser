4a4931a6446f9feeec8cd95ac4f16a3a5a4e824a
helltf <dario.zachow5530@gmail.com>
2023-01-27 09:37:51 +0100

    refactor(navbar): move parts into seperate components

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -1,29 +1,16 @@
-import Image from 'next/image';
-import icon from '../../../public/emote_guesser_icon.gif';
-import NavIcons from './nav-icons';
-import NavLink from './nav-link';
+import NavIcons from "./nav-icons";
+import NavLinks from "./nav-links";
+import NavLogo from "./nav-logo";
 
 export default function Navbar() {
   return (
     <nav className="w-full h-[60px] flex flex-row px-28 items-center justify-between bg-neutral-900 border-b border-gray-500">
       <div className="flex flex-row h-[48px] align-middle gap-2">
-        <Image
-          className=""
-          src={icon}
-          alt="emote guesser icon"
-          width="48"
-          height="48"
-        ></Image>
-        <p className="text-4xl text-center align-middle text-purple-600 italic leading-[48px]">
-          Emote Guesser
-        </p>
+        <NavLogo />
       </div>
-      <div className="flex ">
+      <div className="flex">
         <div className="flex flex-row gap-7">
-          <NavLink href="/">Home</NavLink>
-          <NavLink href="/play">Play</NavLink>
-          <NavLink href="/stats">Stats</NavLink>
-          <NavLink href="/login">Login</NavLink>
+          <NavLinks></NavLinks>
         </div>
         <div>
           <NavIcons></NavIcons>
