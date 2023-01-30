8ea04ef513eb99eccf3f9183c073ea3e320d8e9f
helltf <dario.zachow5530@gmail.com>
2023-01-30 10:07:34 +0100

    chore: rename result type

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,13 +1,13 @@
-export type Option<T> = None | Some<T>;
+export type Result<T> = ResultError | Ok<T>;
 
-export class Some<T> {
+export class Ok<T> {
   data: T;
   constructor(data: T) {
     this.data = data;
   }
 }
 
-export class None {
+export class ResultError {
   error: string;
   constructor(error: string) {
     this.error = error;
