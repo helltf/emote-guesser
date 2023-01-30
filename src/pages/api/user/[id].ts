c989bac2e0bf3fac07cacf22fd20647655eff48a
helltf <dario.zachow5530@gmail.com>
2023-01-30 10:08:15 +0100

    chore: fetching user id

--- src/pages/api/user/[id].ts
+++ src/pages/api/user/[id].ts
@@ -1,3 +1,5 @@
+import { ResultError } from "@/api/types";
+import { request } from "@/api/util";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function emoteHandler(
@@ -7,12 +9,14 @@ export default function emoteHandler(
   res.status(200).json({ forsen: "forsen" });
 }
 
-async function getUserId(username: string): Promise<number> {
-  const data = (await (
-    await fetch("https://api.ivr.fi/v2/twitch/user?login=" + username)
-  ).json()) as UserInfo;
-}
+async function getUserId(username: string): Promise<Result<number>> {
+  const url = "https://api.ivr.fi/v2/twitch/user?login=" + username;
+  const result = await request<UserInfo>(url);
 
+  if (result instanceof ResultError) {
+    return new ResultError("User does not exist");
+  }
+}
 export interface UserInfo {
   id: string;
 }
