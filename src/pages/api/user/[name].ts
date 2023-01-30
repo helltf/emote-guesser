4d5efbedb2d8dfed13544f5fbaa7f38aec0f29d2
helltf <dario.zachow5530@gmail.com>
2023-01-30 19:17:18 +0100

    chore: use name instead of id

--- src/pages/api/user/[name].ts
+++ src/pages/api/user/[name].ts
@@ -1,22 +1,32 @@
-import { ResultError } from "@/api/types";
+import { Ok, Result, ResultError } from "@/api/types";
 import { request } from "@/api/util";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function emoteHandler(
+export default async function emoteHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ forsen: "forsen" });
+  const { name } = req.query;
+  const channelId = await getUserId(name as string);
+
+  if (channelId instanceof ResultError) {
+    return res.status(404).send("Not found");
+  }
+
+  res.status(200).json({ id: channelId.data });
 }
 
 async function getUserId(username: string): Promise<Result<number>> {
   const url = "https://api.ivr.fi/v2/twitch/user?login=" + username;
-  const result = await request<UserInfo>(url);
+  const result = await request<UserInfo[]>(url);
 
   if (result instanceof ResultError) {
     return new ResultError("User does not exist");
   }
+
+  return new Ok(Number(result.data[0].id));
 }
+
 export interface UserInfo {
   id: string;
 }
