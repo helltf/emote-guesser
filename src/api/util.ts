84e56e1c82796e01a43d91cb0f3444d1269e9644
helltf <dario.zachow5530@gmail.com>
2023-01-30 10:08:04 +0100

    chore: add request helper method

--- src/api/util.ts
+++ src/api/util.ts
@@ -1,3 +1,10 @@
-import { Option } from "./types";
+import { ResultError, Result, Ok } from "./types";
 
-export const request = <T>(url: string): Promise<Option<T>> => {};
+export const request = async <T>(url: string): Promise<Result<T>> => {
+  try {
+    const data = (await (await fetch(url)).json()) as T;
+    return new Ok(data);
+  } catch (e: any) {
+    return new ResultError(e);
+  }
+};
