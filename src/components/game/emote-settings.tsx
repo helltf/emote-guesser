62f26845e43c475e2df29e818dd0dfd6646a40a6
helltf <dario.zachow5530@gmail.com>
2023-02-03 13:47:19 +0100

    chore: add hover to emoteset selection

--- src/components/game/emote-settings.tsx
+++ src/components/game/emote-settings.tsx
@@ -10,8 +10,8 @@ export default function EmoteSettingsButton(props: {
       onClick={() => {
         setSelected(!selected);
       }}
-      className={`p-4 px-9 text-white border-[1px] border-neutral-700 rounded-lg flex flex-col cursor-pointer items-center ${
-        selected ? "border-purple-600" : ""
+      className={`p-4 px-9 text-white border-[1px] border-neutral-700 rounded-lg flex flex-col cursor-pointer items-center transition gap-1 ${
+        selected ? "bg-purple-900" : "hover:bg-neutral-600"
       }`}
     >
       <props.icon></props.icon>
