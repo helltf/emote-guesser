0458c637be22faa264649a16983f2b34ac949392
helltf <dario.zachow5530@gmail.com>
2023-02-03 11:51:35 +0100

    feat: display name and icons

--- src/components/game/emote-settings.tsx
+++ src/components/game/emote-settings.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 
-export default function EmoteSettingsButton(props: { children: any }) {
+export default function EmoteSettingsButton(props: {
+  children: any;
+  icon: any;
+}) {
   const [selected, setSelected] = useState(false);
   return (
-    <div>
-      <button
-        className={`p-2 px-4 text-white border-[1px] border-neutral-700 rounded-lg ${
-          selected ? "border-purple-600" : ""
-        }`}
-        onClick={() => {
-          setSelected(!selected);
-        }}
-      >
-        {props.children}
-      </button>
+    <div
+      onClick={() => {
+        setSelected(!selected);
+      }}
+      className={`p-4 px-9 text-white border-[1px] border-neutral-700 rounded-lg flex flex-col cursor-pointer items-center ${
+        selected ? "border-purple-600" : ""
+      }`}
+    >
+      <props.icon></props.icon>
+      <p className="select-none">{props.children}</p>
     </div>
   );
 }
