bc2b77709eb3293a3c2d55bdc926178ff254ab2d
helltf <dario.zachow5530@gmail.com>
2023-02-24 13:07:19 +0100

    chore: stop input on finish

--- src/components/game/emote-input.tsx
+++ src/components/game/emote-input.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 export default function EmoteInput({
   checkEmote,
+  finished,
 }: {
   checkEmote: (emote: string) => boolean;
+  finished: boolean;
 }) {
   const [emoteInput, setEmoteInput] = useState("");
 
   function onEnter(e: any) {
+    if (finished) return;
     if (e.key !== "Enter") return;
 
     const input = e.target.value;
@@ -20,7 +23,7 @@ export default function EmoteInput({
 
   return (
     <input
-      className="w-1/2 outline-none p-2 border-purple-700 rounded-md border-2"
+      className="w-1/2 rounded-md border-2 border-purple-700 p-2 outline-none"
       placeholder="enter emote"
       onChange={(e) => {
         setEmoteInput(e.target.value);
